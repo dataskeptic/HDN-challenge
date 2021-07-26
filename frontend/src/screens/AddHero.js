99a6251710fc43a9732d71346903a40dd917f2e7
fullpistoladev <mateus_henrique2011@outlook.com>
2021-07-26 16:14:17 -0300

    controlled inputs && subimt events from form

--- frontend/src/screens/AddHero.js
+++ frontend/src/screens/AddHero.js
@@ -2,27 +2,37 @@ import React, {useState} from 'react'
 
 const AddHero = () => {
 
-    const [inputList, setInputList] = useState([{ PowerName: ""}]);    
+    const [heroName, setHeroName] = useState('');
+    const [description, setDescription] = useState('');
+    const [powerList, setpowerList] = useState(['']);
 
     // handle input change
     const handleInputChange = (e, index) => {
-    const { name, value } = e.target;
-    const list = [...inputList];
-    list[index][name] = value;
-    setInputList(list);
-  };
-   
-  // handle click event of the Remove button
-  const handleRemoveClick = index => {
-    const list = [...inputList];
-    list.splice(index, 1);
-    setInputList(list);
-  };
-   
-  // handle click event of the Add button
-  const handleAddClick = () => {
-    setInputList([...inputList, { PowerName: ""}]);
-  };
+      const { value } = e.target;
+      const list = [...powerList];
+      list[index] = value;
+      setpowerList(list);
+    };
+
+    // handle click event of the Remove button
+    const handleRemoveClick = (index) => {
+      const list = [...powerList];
+      list.splice(index, 1);
+      setpowerList(list);
+    };
+
+    // handle click event of the Add button
+    const handleAddClick = () => {
+      setpowerList([...powerList, {}]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const hero = {heroName, description, powerList};
+        alert("A new hero has been added")
+        console.log(hero);
+    }
 
     return (
       <>
@@ -35,43 +45,62 @@ const AddHero = () => {
         >
           Add new hero
         </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="text">Hero's name: </label>
-            <input type="text" placeholder="Enter with the hero's name..." />
+            <input
+              type="text"
+              required
+              value={heroName}
+              onChange={(e) => setHeroName(e.target.value)}
+              placeholder="Enter with the hero's name..."
+            />
           </div>
           <div className="form-control">
             <label htmlFor="text">Hero's description: </label>
-            <input type="text" placeholder="Enter with hero's description..." />
+            <textarea
+              type="text"
+              required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Enter with hero's description..."
+            />
           </div>
           <label htmlFor="text">Hero's power: </label>
 
-          {inputList.map((x, i) => {
-
+          {powerList.map((x, i) => {
             return (
               <div className="form-control-row">
-                <input 
-                    type="text"
-                    name="PowerName" 
-                    value={x.PowerName} 
-                    placeholder="Enter with the hero power..."
-                    onChange={e => handleInputChange(e, i)}
-                    />
+                <input
+                  type="text"
+                  required
+                  name="PowerName"
+                  value={x.PowerName}
+                  placeholder="Enter with the hero power..."
+                  onChange={(e) => handleInputChange(e, i)}
+                />
                 <div className="form-control-row">
-                  {inputList.length !== 1 && (
-                    <button 
-                        className="btn-delete"
-                        onClick={() => handleRemoveClick(i)}><i className="fas fa-trash"></i>
+                  {powerList.length !== 1 && (
+                    <button
+                      className="btn-delete"
+                      onClick={() => handleRemoveClick(i)}
+                    >
+                      <i className="fas fa-trash"></i>
                     </button>
-                    
                   )}
-                  {inputList.length - 1 === i && <button className="btn-add" onClick={handleAddClick}><i className="fas fa-plus"></i></button>}
+                  {powerList.length - 1 === i && (
+                    <button className="btn-add" onClick={handleAddClick}>
+                      <i className="fas fa-plus"></i>
+                    </button>
+                  )}
                 </div>
               </div>
             );
           })}
-          <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
-
+          <button className="btn-hero">Add new hero</button>
+           {/* <p>{heroName}</p>
+          <p>{description}</p>
+          <div style={{ marginTop: 20 }}>{JSON.stringify(powerList)}</div>  */}
         </form>
       </>
     );
