5cdb0ccea815697645e19499cd027d6438692028
fullpistoladev <mateus_henrique2011@outlook.com>
2021-07-27 10:56:16 -0300

    adding buttona to hero component

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -8,9 +8,13 @@ const Hero = ( { hero } ) => {
                 
                 <h3>Description:</h3>
                 {hero.description}
-                <h3>Powers</h3>
+                <h3 style={{paddingTop: '10px'}}>Powers</h3>
                 <div className="powers">
                 {hero.powers.map(( p ) => (<p>{p}</p>))}
+                </div>
+                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
+                    <button className="btn-delete"> <i className="fas fa-trash"></i> </button>
                 </div>
             </div>
         </li>
--- frontend/src/screens/HeroesPowers.js
+++ frontend/src/screens/HeroesPowers.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { GlobalContext } from '../context/GlobalState';
+import Hero from '../components/Hero'
 
 const HeroesPowers = () => {
+
+    const [text, setText] = useState('');
+    const [power, setPower] = useState('');
+    const [herosFilter, setHeroesFiler] = useState([{}]);
+    const { hero } = useContext(GlobalContext);
+
+    useEffect(() => {
+     
+        hero.filter( hero => hero.powers.map(( e ) => (e.powers === power ) && setHeroesFiler(...herosFilter, setHeroesFiler)))
+        
+    }, [power])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPower(text)       
+    }
+
     return (
         <div>
-            <h2 style={{color: "white"}}>Heroo pa Hero</h2>
+            <form onSubmit={handleSubmit}>
+                <div style={{display: 'flex'}}>
+                    <input
+                    type="text"
+                    required
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Enter with the hero's name..."
+                    />
+                    <button className="btn-search"><i className="fas fa-search"></i></button>
+                </div>
+            </form>
+                <ul style={{color: 'aliceblue'}}>
+                    { herosFilter.length > 0 ? (herosFilter.map( p => (<Hero key={p.id} hero={p} />))) : '' }
+                </ul>
         </div>
     )
 }
