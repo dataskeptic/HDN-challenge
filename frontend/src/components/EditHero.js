6fb51ebd596d0bfca86120a6df8d56592b2a0682
fullpistoladev <mateus_henrique2011@outlook.com>
2021-07-29 23:06:21 -0300

    editing hero in the ui to the database

--- frontend/src/components/EditHero.js
+++ frontend/src/components/EditHero.js
@@ -1,123 +1,136 @@
-import React, {useContext, useState} from 'react'
-import { GlobalContext } from '../context/GlobalState';
-import Hero from "../components/Hero";
-
-
+import React, { useContext, useState, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const EditHero = ({ hero, edit, handleCancel }) => {
+  const [name, setName] = useState(hero.name || "");
+  const [description, setDescription] = useState(hero.description || "");
+  const [powers, setPowers] = useState(hero.powers || []);
 
+  const { addHero, deleteHero, getHeroes } = useContext(GlobalContext);
 
-    const [name, setName] = useState(hero.name ||'');
-    const [description, setDescription] = useState(hero.description || '');
-    const [powers, setPowers] = useState(hero.powers || []);
+  useEffect(() => {
+    getHeroes();
+    // eslint-disable-next-line
+  }, []);
 
-    const { addHero } = useContext(GlobalContext);
+  // handle input change
+  const handleNameChange = (value) => {
+    setName(value);
+  };
 
-    // handle input change
-    const handleNameChange = (value) => {
-      setName(value);
+  // handle input change
+  const handleDescriptionChange = (value) => {
+    setDescription(value);
+  };
+
+  // handle input change
+  const handlePowersChange = (e, index) => {
+    const { value } = e.target;
+    const list = [...powers];
+    list[index] = value;
+    setPowers(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...powers];
+    list.splice(index, 1);
+    setPowers(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setPowers([...powers, {}]);
+  };
+
+  // Adding new Hero
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    deleteHero(hero._id);
+
+    const editHero = {
+      name,
+      description,
+      powers,
     };
+    addHero(editHero);
+    alert("The hero has been edited");
+    console.log(editHero);
+    handleCancel();
+  };
 
-    // handle input change
-    const handleDescriptionChange = (value) => {
-      setDescription(value)
-    };
-
-    // handle input change
-    const handlePowersChange = (value) => {
-      const list = [...powers, value];
-      setPowers(list);
-    };
-
-    // handle click event of the Remove button
-    const handleRemoveClick = (index) => {
-      const list = [...powers];
-      list.splice(index, 1);
-      setPowers(list);
-    };
-
-    // handle click event of the Add button
-    const handleAddClick = () => {
-      setPowers([...powers, {}]);
-    };
-
-    // Adding new Hero
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-
-
-        /* const newHero = {
-          name, 
-          description, 
-          powers
-        };
-        addHero(newHero);
-        alert("A new hero has been added")
-        console.log(newHero); */
-    }
-
-    return (
-        <div className="hero">
-            <form onSubmit={handleSubmit}>
-          <div className="form-control">
-            <label htmlFor="text">Hero's name: </label>
-            <input
-              type="text"
-              required
-              value={name}
-              onChange={(e) => handleNameChange(e.target.value)}
-              placeholder="Enter with the hero's name..."
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="text">Hero's description: </label>
-            <textarea
-              type="text"
-              required
-              value={description}
-              onChange={(e) => handleDescriptionChange(e.target.value)}
-              placeholder="Enter with hero's description..."
-            />
-          </div>
-          <label htmlFor="text">Hero's power: </label>
-
-          {powers.map((x, i) => {
-            return (
-              <div className="form-control-row">
-                <input
-                  type="text"
-                  required
-                  name="powers"
-                  value={x.power}
-                  placeholder="Enter with the hero power..."
-                  onChange={(e) => handlePowersChange(e)}
-                />
-                <div className="form-control-row">
-                  {powers.length !== 1 && (
-                    <button
-                      className="btn-delete"
-                      onClick={() => handleRemoveClick(i)}
-                    >
-                      <i className="fas fa-trash"></i>
-                    </button>
-                  )}
-                  {powers.length - 1 === i && (
-                    <button className="btn-add" onClick={handleAddClick}>
-                      <i className="fas fa-plus"></i>
-                    </button>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-          <button style={{width:'75px', marginRight: '5px'}} className="btn-hero">Edit Hero</button>
-          <button onClick={() => handleCancel()} style={{width:'75px'}} className="btn-hero">Cancel</button>
-           
-        </form>
-      
+  return (
+    <div className="hero">
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Hero's name: </label>
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => handleNameChange(e.target.value)}
+            placeholder="Enter with the hero's name..."
+          />
         </div>
-    )
-}
+        <div className="form-control">
+          <label htmlFor="text">Hero's description: </label>
+          <textarea
+            type="text"
+            required
+            value={description}
+            onChange={(e) => handleDescriptionChange(e.target.value)}
+            placeholder="Enter with hero's description..."
+          />
+        </div>
+        <label htmlFor="text">Hero's power: </label>
 
-export default EditHero
+        {powers.map((x, i) => {
+          return (
+            <div className="form-control-row">
+              <input
+                type="text"
+                required
+                name="powers"
+                value={x.power}
+                placeholder="Enter with the hero power..."
+                onChange={(e) => handlePowersChange(e, i)}
+              />
+              <div className="form-control-row">
+                {powers.length !== 1 && (
+                  <button
+                    className="btn-delete"
+                    onClick={() => handleRemoveClick(i)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </button>
+                )}
+                {powers.length - 1 === i && (
+                  <button className="btn-add" onClick={handleAddClick}>
+                    <i className="fas fa-plus"></i>
+                  </button>
+                )}
+              </div>
+            </div>
+          );
+        })}
+        <button
+          style={{ width: "75px", marginRight: "5px" }}
+          className="btn-hero"
+          onSubmit={handleSubmit}
+        >
+          Edit Hero
+        </button>
+        <button
+          onClick={() => handleCancel()}
+          style={{ width: "75px" }}
+          className="btn-hero"
+        >
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditHero;
--- frontend/src/screens/AddHero.js
+++ frontend/src/screens/AddHero.js
@@ -1,117 +1,118 @@
-import React, {useState, useContext} from 'react'
-import { GlobalContext } from '../context/GlobalState';
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const AddHero = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [powers, setPowers] = useState([""]);
 
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [powers, setPowers] = useState(['']);
+  const { addHero } = useContext(GlobalContext);
 
-    const { addHero } = useContext(GlobalContext);
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { value } = e.target;
+    const list = [...powers];
+    list[index] = value;
+    setPowers(list);
+  };
 
-    // handle input change
-    const handleInputChange = (e, index) => {
-      const { value } = e.target;
-      const list = [...powers];
-      list[index] = value;
-      setPowers(list);
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...powers];
+    list.splice(index, 1);
+    setPowers(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setPowers([...powers, {}]);
+  };
+
+  // Adding new Hero
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newHero = {
+      name,
+      description,
+      powers,
     };
+    addHero(newHero);
+    alert("A new hero has been added");
+    console.log(newHero);
+  };
 
-    // handle click event of the Remove button
-    const handleRemoveClick = (index) => {
-      const list = [...powers];
-      list.splice(index, 1);
-      setPowers(list);
-    };
+  return (
+    <>
+      <h2
+        style={{
+          color: "white",
+          paddingBottom: "10px",
+        }}
+      >
+        Add new hero
+      </h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Hero's name: </label>
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter with the hero's name..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="text">Hero's description: </label>
+          <textarea
+            type="text"
+            required
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Enter with hero's description..."
+          />
+        </div>
+        <label htmlFor="text">Hero's power: </label>
 
-    // handle click event of the Add button
-    const handleAddClick = () => {
-      setPowers([...powers, {}]);
-    };
-
-    // Adding new Hero
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const newHero = {
-          name, 
-          description, 
-          powers
-        };
-        addHero(newHero);
-        alert("A new hero has been added")
-        console.log(newHero);
-    }
-
-    return (
-      <>
-        <h2
-          style={{
-            color: "white",
-            paddingBottom: "10px",
-          }}
-        >
-          Add new hero
-        </h2>
-        <form>
-          <div className="form-control">
-            <label htmlFor="text">Hero's name: </label>
-            <input
-              type="text"
-              required
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter with the hero's name..."
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="text">Hero's description: </label>
-            <textarea
-              type="text"
-              required
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Enter with hero's description..."
-            />
-          </div>
-          <label htmlFor="text">Hero's power: </label>
-
-          {powers.map((x, i) => {
-            return (
+        {powers.map((x, i) => {
+          return (
+            <div key={i} className="form-control-row">
+              <input
+                type="text"
+                required
+                name="powers"
+                value={x.power}
+                placeholder="Enter with the hero power..."
+                onChange={(e) => handleInputChange(e, i)}
+              />
               <div className="form-control-row">
-                <input
-                  type="text"
-                  required
-                  name="powers"
-                  value={x.power}
-                  placeholder="Enter with the hero power..."
-                  onChange={(e) => handleInputChange(e, i)}
-                />
-                <div className="form-control-row">
-                  {powers.length !== 1 && (
-                    <button
-                      className="btn-delete"
-                      onClick={() => handleRemoveClick(i)}
-                    >
-                      <i className="fas fa-trash"></i>
-                    </button>
-                  )}
-                  {powers.length - 1 === i && (
-                    <button className="btn-add" onClick={handleAddClick}>
-                      <i className="fas fa-plus"></i>
-                    </button>
-                  )}
-                </div>
+                {powers.length !== 1 && (
+                  <button
+                    className="btn-delete"
+                    onClick={() => handleRemoveClick(i)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </button>
+                )}
+                {powers.length - 1 === i && (
+                  <button className="btn-add" onClick={handleAddClick}>
+                    <i className="fas fa-plus"></i>
+                  </button>
+                )}
               </div>
-            );
-          })}
-          <button onClick={handleSubmit} className="btn-hero">Add new hero</button>
-           {/* <p>{hero}</p>
+            </div>
+          );
+        })}
+        <button onClick={handleSubmit} className="btn-hero">
+          Add new hero
+        </button>
+        {/* <p>{hero}</p>
           <p>{description}</p>
           <div style={{ marginTop: 20 }}>{JSON.stringify(powers)}</div>  */}
-        </form>
-      </>
-    );
-}
+      </form>
+    </>
+  );
+};
 
-export default AddHero
+export default AddHero;
